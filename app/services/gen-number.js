2d010ca8db00cf9afeb6da84356dd44da0544133
Alex Hagge and Amanda Daly <techdaly@gmail.com>
2016-06-20 11:11:36 -0700

    Random numbers generating

---
+++ app/services/gen-number.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+
+  randomArray: [],
+
+  getArray() {
+    for ( var i=0; i<4; i++) {
+      this.get('randomArray').push(Math.floor(Math.random() * 4));
+    }
+  }
+
+});
