07254b0e52b0a69d9f0b90eb597855a27dd9938d
Farnoosh Johnson and Luca Quatela <farnoosh@>
2016-06-20 16:24:59 -0700

    stuck in for loop

--- app/services/storing-data.js
+++ app/services/storing-data.js
@@ -5,5 +5,14 @@ export default Ember.Service.extend({
 
   add(userNumberArray) {
     this.set('userNumbers',userNumberArray);
-  }
+    console.log(this.get('userNumbers'));
+  },
+
+  // checkResult: Ember.computed('userNumbers.[]', function(){
+  //   if(this.get('userNumbers').indexOf('1')=== -1 ){
+  //     console.log('number 1 is not exist in the array')
+  //   }
+  //   debugger;
+  //   return "number 1 is not exist in the array";
+  // })
 });
